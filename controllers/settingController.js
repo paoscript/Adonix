8ad5ac9f13d5525e9dca86910abee2678079118f
dicapisar <dicapisar@gmail.com>
2022-07-14 17:53:36 -0500

    Limpieza de código en settingsController

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -2,40 +2,42 @@ var express = require('express');
 var router = express.Router();
 const userManagement = require("../services/userManager");
 
-/* GET login page. */
-router.get('/', async function(req, res, next) {
+/* GET settings page. */
+router.get('/', async (req, res) => {
     let idUser = req.cookies.idUser;
     let idRolUser = req.cookies.idRolUser;
-
-    let user = await userManagement.getUserById(idUser);
 
     if (idUser === undefined) {
         res.redirect("/login");
         return;
-    } else {
-        res.render('settings', 
-            { 
-                title: 'Settings', 
-                isWithInterface: true,
-                isHasMenuUserPermition: idRolUser == 1 ? true : false, 
-                user:user
-            }
-        );
     }
+
+    let user = await userManagement.getUserById(idUser);
+
+    res.render('settings', 
+        { 
+            title: 'Settings', 
+            isWithInterface: true,
+            isHasMenuUserPermition: idRolUser == 1 ? true : false, 
+            user: user
+        }
+    );
+
 });
 
-
-router.post('/', async (req, res, next) => {
+/*POST update setting for requesting user. */
+router.post('/', async (req, res) => {
     let idUser = req.cookies.idUser;
     let { identification, password, name, email} = req.body;
 
     if (idUser === undefined) {
         res.redirect("/login");
         return;
-    } else {
-        await userManagement.updateUserSettingsById(identification, password, name, email, idUser);
-        res.redirect('/settings/');
-    }
+    } 
+
+    await userManagement.updateUserSettingsById(identification, password, name, email, idUser);
+
+    res.redirect('/settings/');
 })
 
 module.exports = router;
