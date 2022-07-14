2ef276516a79fc7001d1e5a4b3af11d7c24a1263
dicapisar <dicapisar@gmail.com>
2022-07-14 17:34:04 -0500

    limpieza de código logoutController

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET main page. */
-router.get('/', function(req, res, next) {
+/* GET go to logout page. */
+router.get('/', (req, res) => {
+
     res.clearCookie('idUser')
+    
     res.render('logout', 
         { 
             title: 'Sesion Cerrada', 
