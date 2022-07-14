d268c22a5be961bbd51b3997cb7422d319dc56f2
dicapisar <dicapisar@gmail.com>
2022-07-14 17:32:32 -0500

    limpieza de código loginController

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const userManagement = require("../services/userManager");
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
     let idUser = req.cookies.idUser;
 
     if (idUser === undefined) {
@@ -20,21 +20,22 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.post('/', async (req, res, next) => {
+/*POST checks if the user can log in to the system.*/
+router.post('/', async (req, res) => {
     
     let { numberIdentification, password} = req.body;
 
     let infoLogin = await userManagement.validateLogin(numberIdentification, password);
 
     if (!infoLogin.isLogin) {
-        res.redirect('/');
+        res.redirect('/login');
         return;
     } 
 
     res.cookie('idUser', infoLogin.idUser);
     res.cookie('idRole', infoLogin.rolId);
-    res.redirect('/');
 
+    res.redirect('/');
 })
 
 module.exports = router;
