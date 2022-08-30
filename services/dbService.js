46b2f278034cd576acaec33330c91c1a3afb1a89
Paola Andrea Castañeda Florez <84241422+paoscript@users.noreply.github.com>
2022-08-29 20:49:53 -0500

    Update dbService.js
    Adding settings connections for the connections pools

--- services/dbService.js
+++ services/dbService.js
@@ -6,7 +6,10 @@ const pool = mysql.createPool({
     host     : process.env.HOST_DATABASE,
     user     : process.env.USER_DATABASE,
     password : process.env.PASSWORD_USER_DATABASE,
-    database : process.env.NAME_DATABASE
+    database : process.env.NAME_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit:0
 })
 
 let query = function (sql, values) {
